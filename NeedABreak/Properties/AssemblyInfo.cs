ca43ca67cd3148b71e25174f69ca2ceb6a62e3de
Benoît Rocco <benoit.rocco@gmail.com>
2020-03-13 20:51:59 +0100

    assemblyInfo update

--- NeedABreak/Properties/AssemblyInfo.cs
+++ NeedABreak/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NeedABreak")]
-[assembly: AssemblyCopyright("Copyright © Benoit Rocco  2016")]
+[assembly: AssemblyCopyright("Copyright © Benoit Rocco  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
