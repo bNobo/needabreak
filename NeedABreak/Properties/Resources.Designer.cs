c55e59500d39bf6f9393170b4a05b88ec979b474
Benoît Rocco <benoit.rocco@gmail.com>
2020-04-10 15:51:19 +0200

    added about box menu item

--- NeedABreak/Properties/Resources.Designer.cs
+++ NeedABreak/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace NeedABreak.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,15 @@ namespace NeedABreak.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to À propos de NEED A BREAK!.
+        /// </summary>
+        internal static string about {
+            get {
+                return ResourceManager.GetString("about", resourceCulture);
             }
         }
         
