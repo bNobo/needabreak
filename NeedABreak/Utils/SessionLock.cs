8651834314c34e5a6a78ab5240ce264ca30c9ded
Benoît Rocco <benoit.rocco@gmail.com>
2020-04-24 19:37:57 +0200

    add missing copyleft on SessionLock

--- NeedABreak/Utils/SessionLock.cs
+++ NeedABreak/Utils/SessionLock.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ NEED A BREAK is an application intended to help you take care of your health while you work on a computer. 
+ It will encourage you to regularly have a break in order to rest your back and your eyes.
+    Copyright (C) 2020  Benoît Rocco
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +26,7 @@ using System.Windows;
 
 namespace NeedABreak.Utils
 {
-    class SessionLock
+    static class SessionLock
     {
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -16,7 +34,7 @@ namespace NeedABreak.Utils
 
         public static void LockSession()
         {
-            if (LockWorkStation() == false)
+            if (!LockWorkStation())
             {
                 MessageBox.Show("Session Could not be locked!");
             }
