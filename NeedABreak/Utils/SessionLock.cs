1e536bec42d1ce84082ddd084eea9be93b517079
Benoît Rocco <benoit.rocco@gmail.com>
2020-05-01 16:17:20 +0200

    explicitly specify access modifiers

--- NeedABreak/Utils/SessionLock.cs
+++ NeedABreak/Utils/SessionLock.cs
@@ -24,14 +24,15 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace NeedABreak.Utils
 {
-    static class SessionLock
+    public static class SessionLock
     {
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool LockWorkStation();
+        private static extern bool LockWorkStation();
 
         public static void LockSession()
         {
